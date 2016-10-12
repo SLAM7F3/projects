b6f3c2b1e24b250b5c73df73084323a165eda4b0
Peter Cho <pcho@apple.com>
2016-10-12 06:59:41 -0700

    Adding fill_grid.cc to repo

---
+++ src/mains/games/fill_grid.cc
@@ -0,0 +1,58 @@
+// ==========================================================================
+// Program FILL_GRID
+// ==========================================================================
+// Last updated on 10/12/16
+// ==========================================================================
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "machine_learning/reinforce.h"
+#include "games/tictac3d.h"
+#include "numrec/nrfuncs.h"
+#include "general/outputfuncs.h"
+
+int main (int argc, char* argv[])
+{
+   using std::cin;
+   using std::cout;
+   using std::endl;
+   using std::string;
+   using std::vector;
+
+   nrfunc::init_time_based_seed();
+   tictac3d* ttt_ptr = new tictac3d(4,1);
+
+   reinforce* reinforce_ptr = new reinforce();
+   cout << "*reinforce_ptr = " << *reinforce_ptr << endl;
+   reinforce_ptr->initialize_episode();
+
+   while(!ttt_ptr->get_game_over())
+   {
+//       ttt_ptr->randomize_board_state();
+      ttt_ptr->display_board_state();
+      
+      ttt_ptr->enter_human_move();
+
+/*
+      if(ttt_ptr->check_player_win(1))
+      {
+         game_over = true;
+      }
+      
+      if(ttt_ptr->check_player_win(-1))
+      {
+         game_over = true;
+      }
+*/
+
+   } // !game_over while loop
+   cout << "Final score = " << ttt_ptr->get_score() << endl;
+   cout << "GAME OVER" << endl << endl;
+
+   delete ttt_ptr;
+   delete reinforce_ptr;
+}
+
+
+
