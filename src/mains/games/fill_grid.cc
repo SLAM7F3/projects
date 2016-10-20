4cbac75457de4d538c5638479f975be98ffbcce8
Peter Cho <pcho@apple.com>
2016-10-20 13:54:13 -0700

    Updated README.reinforce

--- src/mains/games/fill_grid.cc
+++ src/mains/games/fill_grid.cc
@@ -27,8 +27,9 @@ int main (int argc, char* argv[])
 
    int nsize = 4;
 //   int n_zlevels = 1;
-   int n_zlevels = 2;
+//   int n_zlevels = 2;
 //   int n_zlevels = 3;
+   int n_zlevels = 4;
    tictac3d* ttt_ptr = new tictac3d(nsize, n_zlevels);
 
    int Din = nsize * nsize * n_zlevels;	// Input dimensionality
@@ -45,8 +46,8 @@ int main (int argc, char* argv[])
    layer_dims.push_back(Dout);
    reinforce* reinforce_ptr = new reinforce(layer_dims, Tmax);
 
-//   int n_max_episodes = 1 * 1000000;
-   int n_max_episodes = 2 * 1000000;
+   int n_max_episodes = 1 * 1000000;
+//   int n_max_episodes = 2 * 1000000;
    int n_update = 0.01 * n_max_episodes;
    int n_losses = 0;
    int n_wins = 0;
