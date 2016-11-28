5fa9ba5ccbece37af260dc71a39cca164e1682aa
Peter Cho <pcho@apple.com>
2016-11-28 14:06:06 -0800

    Updated README.TTT with SGD results

--- src/mains/games/qtrain_ttt_network.cc
+++ src/mains/games/qtrain_ttt_network.cc
@@ -146,8 +146,9 @@ int main (int argc, char* argv[])
    reinforce_agent_ptr->set_gamma(gamma);  
    reinforce_agent_ptr->set_rmsprop_decay_rate(0.90);
 
-   reinforce_agent_ptr->set_base_learning_rate(1E-4);
-//   reinforce_agent_ptr->set_base_learning_rate(1E-5);
+//   reinforce_agent_ptr->set_base_learning_rate(1E-3);
+//   reinforce_agent_ptr->set_base_learning_rate(1E-4);
+   reinforce_agent_ptr->set_base_learning_rate(3E-5);
 //   reinforce_agent_ptr->set_base_learning_rate(1E-6);
 
 //   reinforce_agent_ptr->set_base_learning_rate(3E-6);
