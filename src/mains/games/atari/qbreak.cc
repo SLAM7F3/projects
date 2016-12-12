a7bea40378a76333732aca37e66ff68af1d6a889
cho <cho@kermie.(none)>
2016-12-12 18:51:42 -0500

    Running 3 more expts on m6700

--- src/mains/games/atari/qbreak.cc
+++ src/mains/games/atari/qbreak.cc
@@ -66,18 +66,18 @@ int main(int argc, char** argv)
 
 //   int H1 = 32;
 //   int H1 = 64;
-   int H1 = 128;
+   int H1 = 64;
 //   int H1 = 256;
 
 //   int H2 = 0;
 //   int H2 = 8;
 //   int H2 = 16;
 //   int H2 = 32;
-   int H2 = 64;
+   int H2 = 128;
 
-//   int H3 = 0;
+   int H3 = 0;
 //   int H3 = 16;
-   int H3 = 32;
+//   int H3 = 32;
 
    vector<int> layer_dims;
    layer_dims.push_back(Din);
