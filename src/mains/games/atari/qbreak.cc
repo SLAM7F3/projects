5158194b710e6c95b64a5e76aa286882a31889a1
Peter Cho <pcho@apple.com>
2016-12-16 16:07:59 -0800

    Running 5 expts on TM, T1 and T3

--- src/mains/games/atari/qbreak.cc
+++ src/mains/games/atari/qbreak.cc
@@ -68,16 +68,16 @@ int main(int argc, char** argv)
    int Dout = n_actions;
 
 //   int H1 = 32;
-   int H1 = 64;
-//   int H1 = 128;
+//   int H1 = 64;
+   int H1 = 128;
 //   int H1 = 200;
 //   int H1 = 256;
 
 //   int H2 = 0;
 //   int H2 = 16;
-//   int H2 = 32;
+   int H2 = 32;
 //   int H2 = 64;
-   int H2 = 128;
+//   int H2 = 128;
 
    int H3 = 0;
 //   int H3 = 16;
