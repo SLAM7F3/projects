4409c29c6e0d37ce91ef977f6583710114b5df99
cho <cho@kermie.(none)>
2016-12-30 11:27:14 -0500

    Try larger learning rate for ppong

--- src/mains/games/atari/ppong.cc
+++ src/mains/games/atari/ppong.cc
@@ -76,10 +76,10 @@ int main(int argc, char** argv)
 //   int H1 = 128;
 //   int H1 = 200;
 
-   int H2 = 0;
+//   int H2 = 0;
 //   int H2 = 8;
 //   int H2 = 16;
-//   int H2 = 32;
+   int H2 = 32;
 //   int H2 = 64;
 //   int H2 = 128;
 
@@ -138,8 +138,8 @@ int main(int argc, char** argv)
 //   reinforce_agent_ptr->set_rmsprop_decay_rate(0.95);
 
 //   reinforce_agent_ptr->set_base_learning_rate(3E-3);
-//   reinforce_agent_ptr->set_base_learning_rate(1E-3);
-   reinforce_agent_ptr->set_base_learning_rate(3E-4);  
+   reinforce_agent_ptr->set_base_learning_rate(1E-3);
+//   reinforce_agent_ptr->set_base_learning_rate(3E-4);  
 //   reinforce_agent_ptr->set_base_learning_rate(1E-4);  
 
    int n_lr_episodes_period = 10 * 1000;
