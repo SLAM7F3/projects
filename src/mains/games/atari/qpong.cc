7c60390f2ab2441972454af2865d829332ed3d12
Peter Cho <pcho@apple.com>
2016-12-30 16:35:31 -0800

    Running 3 * 6 = 18 pong expts

--- src/mains/games/atari/qpong.cc
+++ src/mains/games/atari/qpong.cc
@@ -84,10 +84,10 @@ int main(int argc, char** argv)
    int H1 = 64;
 //   int H1 = 128;
 
-   int H2 = 0;
+//   int H2 = 0;
 //   int H2 = 8;
 //   int H2 = 16;
-//   int H2 = 32;
+   int H2 = 32;
 //   int H2 = 64;
 //   int H2 = 128;
 
